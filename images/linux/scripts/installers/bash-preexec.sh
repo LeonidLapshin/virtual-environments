4c6817871d17ea24231de12838a3bc8b301204aa
Leonid Lapshin <originalnoe-nazvanie@yandex.ru>
2020-10-20 14:55:07 +0300

    syntax error

--- images/linux/scripts/installers/bash-preexec.sh
+++ images/linux/scripts/installers/bash-preexec.sh
@@ -8,7 +8,7 @@ fi
 
 preexec() {
     set +e
-    APTPID=( $(ps -ef  | grep "[a]pt" | awk '{ printf "%d ", $2 }') )
+    APTPID=$(ps -ef  | grep "[a]pt" | awk '{ printf "%d ", $2 }')
     if [[ ! -z "$APTPID" ]]
     then
         for ONEPID in "${APTPID[@]}"
@@ -22,7 +22,7 @@ preexec() {
         echo "apt/apt-get is not running"
     fi
 
-    DPKGPID=( $(ps -ef  | grep "[d]pkg" | awk '{ printf "%d ", $2 }') )
+    DPKGPID=$(ps -ef  | grep "[d]pkg" | awk '{ printf "%d ", $2 }')
     if [[ ! -z "$DPKGPID" ]]
     then
         for ONEPID in "${DPKGPID[@]}"
