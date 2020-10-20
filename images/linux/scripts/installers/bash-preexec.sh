e0a6650c83493e3fe1b01408896b890d9d4c0629
Leonid Lapshin <originalnoe-nazvanie@yandex.ru>
2020-10-20 16:29:33 +0300

    set bash-preexec as last

--- images/linux/scripts/installers/bash-preexec.sh
+++ images/linux/scripts/installers/bash-preexec.sh
@@ -3,8 +3,8 @@ if [ -f "$HOME/.bash-preexec.sh" ] ; then
 else
     echo "initial setup bashrc"
     curl https://raw.githubusercontent.com/rcaloras/bash-preexec/master/bash-preexec.sh -o ~/.bash-preexec.sh
-    echo '[[ -f ~/.bash-preexec.sh ]] && source ~/.bash-preexec.sh' >> ~/.bashrc
     cp -f /imagegeneration/installers/bash-preexec-function.sh ${HOME}/.bash-preexec-function.sh
     ls -la ${HOME}
     echo '[[ -f ~/.bash-preexec-function.sh ]] && source ~/.bash-preexec-function.sh' >> ~/.bashrc
+    echo '[[ -f ~/.bash-preexec.sh ]] && source ~/.bash-preexec.sh' >> ~/.bashrc
 fi
